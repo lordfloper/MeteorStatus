9b52a24b68436383bc29c7ab8849afc4b94cf1cb
Aldana <adrianarturo30@gmail.com>
2023-06-26 13:25:07 -0300

    created the message variable, to send email notifications

--- handler/MeteorsStatus.py
+++ handler/MeteorsStatus.py
@@ -1,24 +1,30 @@
 import json
 import urllib.parse
+import os
 import boto3 
 
 TABLENAME = "Potentially_hazardous_asteroid",
 MAX_DISTANCE_CHANGE = 10
 MAX_SPEED_CHANGE = 10
 
+SNS_TOPIC_ARN = os.getenv("SNS_TOPIC_ARN")
 
 def check_hazard_status(name, speed, distance):
-    dynamodb = boto3.client("dynamodb")
+    # dynamodb = boto3.client("dynamodb")
+    sns = boto3.client("sns")
 
-    response = dynamodb.get_item(
-        TableName = TABLENAME,
-        Key = {'name': {'S': name} }
+    # response = dynamodb.get_item(
+    #     TableName = TABLENAME,
+    #     Key = {'name': {'S': name} }
+    # )
+
+    # print(response['Item'])
+
+    sns.publish(
+        TopicArn = SNS_TOPIC_ARN,
+        Subject = "Hazard! Warning!",
+        Message = f"Dangerous, dangerous asteroid {name}. Distance {distance}. Speed {speed}."
     )
-
-    print(response['Item'])
-    
-
-    
 
 def lambda_handler(event, context):
     # TODO implement
